1e9b7cf5dc52b797e91611bfd1dd0edfdc7a39d0
Artur Luniaka <artur01049718@gmail.com>
2024-08-09 08:08:16 +0100

    finish HW 06

--- js/task-3.js
+++ js/task-3.js
@@ -1,42 +1,28 @@
-const sortByDescendingFriendCount = (users) => {
-    return users.toSorted((less, more) => more.friends.length - less.friends.length);
-}
-console.log(
-  sortByDescendingFriendCount([
-    {
-      name: "Moore Hensley",
-      friends: ["Sharron Pace"],
-      gender: "male"
-    },
-    {
-      name: "Sharlene Bush",
-      friends: ["Briana Decker", "Sharron Pace"],
-      gender: "female"
-    },
-    {
-      name: "Ross Vazquez",
-      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-      gender: "male"
-    },
-    {
-      name: "Elma Head",
-      friends: ["Goldie Gentry", "Aisha Tran"],
-      gender: "female"
-    },
-    {
-      name: "Carey Barr",
-      friends: ["Jordan Sampson", "Eddie Strong"],
-      gender: "male"
-    },
-    {
-      name: "Blackburn Dotson",
-      friends: ["Jacklyn Lucas", "Linda Chapman"],
-      gender: "male"
-    },
-    {
-      name: "Sheree Anthony",
-      friends: ["Goldie Gentry", "Briana Decker"],
-      gender: "female"
+class StringBuilder {
+    #value
+    constructor(initialValue) {
+        this.#value = initialValue
     }
-  ])
-);
+    getValue() {
+        return this.#value
+    }
+    padEnd(str) {
+        // this.#value = this.#value.padEnd(this.#value.length + str.length, str)
+       this.#value = this.#value + str
+    }
+    padStart(str) {
+        // this.#value = this.#value.padStart(this.#value.length + str.length, str)
+       this.#value = str + this.#value
+    }
+    padBoth(str) {
+        this.#value = str + this.#value + str
+    }
+}
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
